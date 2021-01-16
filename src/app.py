6161b60dcc3be8df9c87a37ea6bd4c9445a2b12f
milton-reyes <altf4616@outlook.com>
2021-01-16 08:18:23 -0500

    basic stuff to app

--- src/app.py
+++ src/app.py
@@ -2,5 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index page'
+    
+@app.route('/hellow')
 def hello_world():
     return 'Hello World'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, world'
